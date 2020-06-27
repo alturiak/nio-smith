0a9fc7235bb67c5c07e11ba3d893436dd4be82b1
alturiak <1785173+alturiak@users.noreply.github.com>
2020-06-27 17:31:27 +0200

    sampleplugin.py: actually produce sample output

--- plugins/sampleplugin.py
+++ plugins/sampleplugin.py
@@ -1,8 +1,9 @@
 from plugin import Plugin
+from chat_functions import send_text_to_room
 
 
 async def sample_command(command):
-    print("Sample debug output")
+    await send_text_to_room(command.client, command.room.room_id, "This is a sample output")
 
 plugin = Plugin("Sample", "General", "Just a simple sample.")
-plugin.add_command("sample", sample_command, "A simple sample command")
+plugin.add_command("sample", sample_command, "A simple sample command, producing a simple sample output")
