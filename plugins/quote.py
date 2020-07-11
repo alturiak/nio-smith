2199083a3f2bfaf419e4f2ee6bfe738580065710
alturiak <1785173+alturiak@users.noreply.github.com>
2020-07-11 22:09:51 +0200

    Store reactions to quotes, update documentation

--- plugins/quote.py
+++ plugins/quote.py
@@ -63,12 +63,18 @@ class Quote:
         :return: the textual representation of the quote
         """
 
-        quote_text: str = self.text.replace("|", "\n")
-        reactions_text: str = ""
-        for reaction, count in self.reactions:
-            reactions_text += f"{reaction} ({count})"
+        quote_text: str = self.text.replace("|", "  \n")
+        quote_text = quote_text.replace("<", "&lt;")
+        quote_text = quote_text.replace(">", "&gt;")
 
-        return f"**Q {self.id}**:\n{quote_text}\n\n{reactions_text}"
+        reactions_text: str = ""
+        for reaction, count in self.reactions.items():
+            if count == 1:
+                reactions_text += f"{reaction} "
+            else:
+                reactions_text += f"{reaction}({count}) "
+
+        return f"**Quote {self.id}**:  \n{quote_text}  \n{reactions_text}"
 
     async def display_details(self) -> str:
         """
@@ -76,7 +82,7 @@ class Quote:
         :return: the detailed textual representation of the quote
         """
 
-        full_text: str = f"{self.display_text()}\n\n" \
+        full_text: str = f"{self.display_text()}\n  " \
                          f"Date: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(self.date))}\n" \
                          f"Added by: {self.user} / {self.mxuser}\n" \
                          f"Added on: {self.chan} / {self.mxroom}\n" \
@@ -84,12 +90,30 @@ class Quote:
         return full_text
 
     async def match(self, search_terms: List[str]) -> bool:
+        """
+        Check if the quote matches a list of search terms
+        :param search_terms: list of search terms
+        :return:    True, if it matches
+                    False, if it does not match
+        """
 
         search_term: str
         for search_term in search_terms:
             if search_term.lower() not in self.text.lower():
                 return False
         return True
+
+    async def quote_add_reaction(self, reaction: str):
+        """
+        Add a reaction to a quote
+        :param reaction: the reaction that should be added to the quote
+        :return:
+        """
+
+        if reaction in self.reactions.keys():
+            self.reactions[reaction] += 1
+        else:
+            self.reactions[reaction] = 1
 
 
 async def quote_command(command):
@@ -109,8 +133,6 @@ async def quote_command(command):
         await plugin.reply_notice(command, "Error: no quotes stored")
         return False
 
-    print(f"Quotes: {quotes}")
-
     quote_id: int
     quote: Quote
 
@@ -120,12 +142,11 @@ async def quote_command(command):
         await plugin.reply(command, await quote.display_text())
 
     elif len(command.args) == 1 and command.args[0].isdigit():
-        print(f"Quotes stored: {len(quotes)}, Quote requested: {command.args[0]}, Type: {type(command.args[0])}")
         """specific quote requested by id"""
-        try:
-            quote = quotes[int(command.args[0])]
+
+        if quote := await find_quote_by_id(quotes, int(command.args[0])):
             await plugin.reply(command, await quote.display_text())
-        except KeyError:
+        else:
             await plugin.reply_notice(command, f"Quote {command.args[0]} not found")
 
     else:
@@ -150,7 +171,7 @@ async def quote_command(command):
             await plugin.reply_notice(command, f"No quote found matching {terms}")
 
 
-async def find_quote_by_search_term(quotes: Dict[int, Quote], terms: List[str], match_id: int = 0) -> Quote:
+async def find_quote_by_search_term(quotes: Dict[int, Quote], terms: List[str], match_id: int = 0) -> Quote or None:
     """
     Search for a matching quote by search terms
     :param quotes: Dict of quotes
@@ -174,19 +195,50 @@ async def find_quote_by_search_term(quotes: Dict[int, Quote], terms: List[str], 
         return None
 
 
-async def find_quote_by_attributes(quotes: Dict[int, Quote]) -> Quote:
+async def find_quote_by_id(quotes: Dict[int, Quote], quote_id: int) -> Quote or None:
+    """
+    Find a quote by its id
+    :param quotes: The dict containing all current quotes
+    :param quote_id: the id of the quote to find
+    :return: the Quote that has been found, None otherwise
+    """
+    try:
+        quote: Quote = quotes[quote_id]
+        return quote
+    except KeyError:
+        return None
+
+
+async def find_quote_by_attributes(quotes: Dict[int, Quote], attribute: str, values: List[str]) -> Quote or None:
+    """
+    Find a quote by its attributes
+    :param quotes: The dict containing all current quotes
+    :param attribute: the attribute by which to find the quote
+    :param values: the values of the attribute the quote has to match
+    :return: the Quote that has been found, None otherwise
+    """
 
     # TODO: implement this :)
-    pass
+    return None
 
 
 async def quote_detail_command(command):
+    """
+    Display a detailed output of the quote
+    :param command:
+    :return:
+    """
 
     # TODO: implement this :)
     pass
 
 
 async def quote_add_command(command):
+    """
+    Add a new quote
+    :param command:
+    :return:
+    """
 
     if len(command.args) > 0:
         quotes: Dict[int, Quote]
@@ -261,8 +313,36 @@ async def quote_restore_command(command):
         await plugin.reply(command, f"Usage: quote_restore <id>")
 
 
-plugin.add_command("testquote", quote_command, "Post quotes, either randomly, by id, or by search string")
+async def quote_add_reaction_command(command):
+    """
+    Add a reaction to a quote
+    :param command:
+    :return:
+    """
+
+    quotes: Dict[int, Quote]
+    try:
+        quotes = plugin.read_data("quotes")
+    except KeyError:
+        quotes = {}
+
+    if len(command.args) == 2 and command.args[0].isdigit():
+        quote: Quote = await find_quote_by_id(quotes, int(command.args[0]))
+        if quote:
+            await quote.quote_add_reaction(command.args[1])
+            quotes[quote.id] = quote
+            plugin.store_data("quotes", quotes)
+            await plugin.reply_notice(command, f"Reaction {command.args[1]} added to quote {command.args[0]}")
+        else:
+            await plugin.reply_notice(command, f"Quote {command.args[0]} not found")
+
+    else:
+        await plugin.reply_notice(command, f"Usage: quote_add_reaction <quote_id> <emoji>")
+
+
+plugin.add_command("quote", quote_command, "Post quotes, either randomly, by id, or by search string")
 # plugin.add_command("quote_detail", quote_detail_command, "View a detailed output of a specific quote")
-plugin.add_command("testquote_add", quote_add_command, "Add a quote")
-plugin.add_command("testquote_del", quote_delete_command, "Delete a quote (can be restored)")
-plugin.add_command("testquote_restore", quote_restore_command, "Restore a quote")
+plugin.add_command("quote_add", quote_add_command, "Add a quote")
+plugin.add_command("quote_del", quote_delete_command, "Delete a quote (can be restored)")
+plugin.add_command("quote_restore", quote_restore_command, "Restore a quote")
+plugin.add_command("quote_add_reaction", quote_add_reaction_command, "Add a reaction to a quote - to be replaced by automatic reaction detection later")
