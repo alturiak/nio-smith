def710caa395a4fea1101b0c08958128a2a35853
alturiak <1785173+alturiak@users.noreply.github.com>
2020-12-25 16:53:16 +0100

    echo.py: Remove room restriction

--- plugins/echo.py
+++ plugins/echo.py
@@ -1,13 +1,15 @@
-from chat_functions import send_typing
 from plugin import Plugin
+
+plugin = Plugin("echo", "General", "A very simple Echo plugin")
+
+
+def setup():
+    plugin.add_command("echo", echo, "make someone agree with you for once")
 
 
 async def echo(command):
     """Echo back the command's arguments"""
     response = " ".join(command.args)
-    await send_typing(command.client, command.room.room_id, response)
+    await plugin.reply(command, response)
 
-
-plugin = Plugin("echo", "General", "A very simple Echo plugin")
-plugin.add_command("echo", echo, "make someone agree with you for once", room_id=["!iAxDarGKqYCIKvNSgu:pack.rocks", "!WLzPcYhULnmsuPwZSN:pack.rocks"])
-plugin.add_command("echo2", echo, "make someone agree with you for once - again", room_id=["!iAxDarGKqYCIKvNSgu:pack.rocks", "!WLzPcYhULnmsuPwZSN:pack.rocks"])
+setup()
